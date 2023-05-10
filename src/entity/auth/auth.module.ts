aace35fea8ee541826affe53bfc644204a0ce0af
Rinat Safin <rinat.safin@digitallyinspired.com>
2023-05-10 16:53:11 +0100

    feat(04): add JwtStrategy class using PassportStrategy and JWT_SECRET

--- src/entity/auth/auth.module.ts
+++ src/entity/auth/auth.module.ts
@@ -4,13 +4,14 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from '@/entity/prisma';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategy';
 
 @Module({
   imports: [
     JwtModule.register({ secret: process.env.JWT_SECRET }),
     PrismaModule,
   ],
-  providers: [AuthResolver, AuthService],
+  providers: [AuthResolver, AuthService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
