27800145cb1054fd45c95c550a42c4ab59c6e125
Rinat Safin <rinat.safin@digitallyinspired.com>
2023-05-11 17:32:17 +0100

    feat(05): add test-db in docker-compose.yml file
    feat(05): add config jest.e2e.js, jest.config.js, .env.test files
    
    fix(05): fix app.e2e-spec.ts

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/',
+  }),
+  // cache: false,
+  rootDir: '.',
+  testEnvironment: 'node',
+  testRegex: '.spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+};
